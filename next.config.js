2bdf1a00d7cefde04d47e807c783674733e8eaa6
Medina FM <medina.fm.167@gmail.com>
2024-11-26 12:44:01 +0700

    next.config.js diedit biar bisa live preview contentful

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 module.exports = {
-    images:{
-        domains:['images.ctfassets.net']
-    }
-}
+    images: {
+      domains: ['images.ctfassets.net'],
+    },
+    async headers() {
+      return [
+        {
+          source: '/(.*)', // Apply headers to all routes
+          headers: [
+            {
+              key: 'Content-Security-Policy',
+              value: "frame-ancestors 'self' https://app.contentful.com",
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
